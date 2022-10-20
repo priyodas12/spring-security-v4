7bca30011fa2a4b79e6df854afe8d6960da18558
Priyobrato.Das <priyodas12@gmail.com>
2022-10-20 13:16:55 +0530

    Initial commit

---
+++ src/main/java/io/springboot/security/springsecurityv4/SpringSecurityV4Application.java
@@ -0,0 +1,13 @@
+package io.springboot.security.springsecurityv4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityV4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityV4Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/springboot/security/springsecurityv4/SpringSecurityV4ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springboot.security.springsecurityv4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityV4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
